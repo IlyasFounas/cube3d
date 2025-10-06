fdcfd8493aacf00464c06faa422ffcbf0a5932d8
Ilyas Founas <ifounas@student.42lyon.fr>
2025-10-06 18:18:25 +0200

    textures are'nt shown proprely

--- srcs/graphic_rendering/texture_rendering.c
+++ srcs/graphic_rendering/texture_rendering.c
@@ -12,7 +12,7 @@ void	wall_rendering(t_global_infos *infos, t_data *img, int i, int y)
 	int	position;
 	int	color;
 
-	position = (infos->tex_y * img->line_length) + (infos->tex_x
+	position = (infos->tex_x * img->line_length) + (infos->tex_y
 			* (img->bits_per_pixel / 8));
 	color = *(int *)(infos->textures->north_data + position);
 	my_mlx_pixel_put(img, i, y, color);
