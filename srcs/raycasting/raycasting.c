3949a22bf0a7aa5ceb3df9268053a9b50819f4e5
Ilyas Founas <ifounas@student.42lyon.fr>
2025-10-22 10:13:31 +0200

    big refactor of the calculs of ray_angle and p_angle + the fisheye + textures rendering

--- srcs/raycasting/raycasting.c
+++ srcs/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 12:01:38 by ifounas           #+#    #+#             */
-/*   Updated: 2025/10/22 10:11:03 by ifounas          ###   ########.fr       */
+/*   Updated: 2025/10/22 10:12:58 by ifounas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,12 @@
 
 static void	calculs_of_vectors(t_global_infos *infos, int i)
 {
-	// double	camera_x;
-	
-	double ratio = (double)i / (double)infos->map_infos->width;
-	int fov = 60.0 * M_PI / 180.0; 
+	double	ratio;
+	int		fov;
+
+	ratio = (double)i / (double)infos->map_infos->width;
+	fov = 60.0 * M_PI / 180.0;
 	infos->ray_angle = infos->p_angle - (fov / 2) + (ratio * fov);
-	// infos->ray_angle = atan2(infos->ray_infos->ray_dir_y,
-	// 		infos->ray_infos->ray_dir_x);
-	// camera_x = 2 * i / (double)infos->map_infos->width - 1;
-	// infos->ray_infos->ray_dir_x = infos->ray_infos->dir_x
-	// 	+ infos->ray_infos->plane_x * camera_x;
-	// infos->ray_infos->ray_dir_y = infos->ray_infos->dir_y
-	// 	+ infos->ray_infos->plane_y * camera_x;
 	infos->ray_infos->ray_dir_x = cos(infos->ray_angle);
 	infos->ray_infos->ray_dir_y = sin(infos->ray_angle);
 }
