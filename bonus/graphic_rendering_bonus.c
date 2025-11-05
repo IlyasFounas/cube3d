1ed3dfcf45dc7699679dc7c6733e9a98df2d394a
Ilyas Founas <ifounas@student.42lyon.fr>
2025-11-05 15:24:12 +0100

    no more invalid

--- bonus/graphic_rendering_bonus.c
+++ bonus/graphic_rendering_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 10:37:55 by ifounas           #+#    #+#             */
-/*   Updated: 2025/11/05 14:12:36 by ifounas          ###   ########.fr       */
+/*   Updated: 2025/11/05 15:21:31 by ifounas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,14 @@ void	wall_rendering_bonus(t_global_infos *infos, t_data *img, int i, int y)
 	while (ptr && ptr->type != infos->actual_orientation)
 		ptr = ptr->next;
 	img_texture = &ptr->data;
+	if (infos->tex_x < 0)
+		infos->tex_x = 0;
+	if (infos->tex_y < 0)
+		infos->tex_y = 0;
+	if (infos->tex_x >= ptr->t_width)
+		infos->tex_x = ptr->t_width - 1;
+	if (infos->tex_y >= ptr->t_height)
+		infos->tex_y = ptr->t_height - 1;
 	position = (infos->tex_y * img_texture->line_length) + (infos->tex_x
 			* (img_texture->bits_per_pixel * 0.125));
 	color = *(int *)(img_texture->addr + position);
