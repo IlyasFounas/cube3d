e597c73be38d820368b0086550e1408b4ee3022d
Ilyas Founas <ifounas@student.42lyon.fr>
2025-11-07 11:32:17 +0100

    the mouse is correctly working

--- bonus/mouse_bonus.c
+++ bonus/mouse_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 10:01:42 by ifounas           #+#    #+#             */
-/*   Updated: 2025/11/06 17:50:56 by ifounas          ###   ########.fr       */
+/*   Updated: 2025/11/07 11:31:59 by ifounas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,14 @@ static void	update_player_rotation_bonus(t_global_infos *infos, double angle)
 	ray->plane_y = new_y;
 }
 
+static void	replace_y(t_global_infos *infos, int y, int x)
+{
+	if (y > infos->map_infos->height)
+		mlx_mouse_move(infos->mlx, infos->mlx_win, x, infos->map_infos->height);
+	else if (y < 0)
+		mlx_mouse_move(infos->mlx, infos->mlx_win, x, 0);
+}
+
 int	handle_mouse_movements(t_global_infos *infos)
 {
 	static int	old_x;
@@ -42,20 +50,20 @@ int	handle_mouse_movements(t_global_infos *infos)
 	mlx_mouse_get_pos(infos->mlx, infos->mlx_win, &x, &y);
 	if (old_x == 0)
 		old_x = x;
-	if (y > 0 && x < infos->map_infos->width && y < infos->map_infos->height)
-	{
-		offset = old_x - x;
-		if (offset < 0)
-			update_player_rotation_bonus(infos, -offset * 0.002);
-		else
-			update_player_rotation_bonus(infos, -offset * 0.002);
-		if (x == infos->map_infos->width - 1)
-			mlx_mouse_move(infos->mlx, infos->mlx_win, 3,
-				infos->map_infos->height / 2);
-		else if (x < 2)
-			mlx_mouse_move(infos->mlx, infos->mlx_win, infos->map_infos->width
-				- 2, infos->map_infos->height / 2);
-		old_x = x;
-	}
+	offset = old_x - x;
+	if (offset < 0 && offset > -(infos->map_infos->width - 30)
+		&& offset < (infos->map_infos->width - 30))
+		update_player_rotation_bonus(infos, -offset * 0.002);
+	else if (offset > -(infos->map_infos->width - 30)
+		&& offset < (infos->map_infos->width - 30))
+		update_player_rotation_bonus(infos, -offset * 0.002);
+	old_x = x;
+	replace_y(infos, y, x);
+	if (x >= infos->map_infos->width || x > infos->map_infos->width - 5)
+		mlx_mouse_move(infos->mlx, infos->mlx_win, 3, infos->map_infos->height
+			/ 2);
+	else if (x <= 0 || x < 2)
+		mlx_mouse_move(infos->mlx, infos->mlx_win, infos->map_infos->width - 6,
+			infos->map_infos->height / 2);
 	return (0);
 }
